c7a7eed74872d10fe496a566273b01606a89be82
Leslie-Cynthia <lesliecynthia1@gmail.com>
2025-04-05 22:53:21 +0000

    Initial commit : scraper.sh, data.csv et README

---
+++ scraper.sh
@@ -0,0 +1,13 @@
+response=$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur")
+
+price=$(echo "$response" | grep -oP '"eur":\K[0-9]+(\.[0-9]+)?')
+
+echo "Prix scrappe : $price EUR"
+
+
+if [ ! -f data.csv ]; then
+	echo "timestamp,price" > data.csv
+fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S'),$price" >> data.csv
+
